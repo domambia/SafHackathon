e16bacd3afee63dabb274975fffe01ade1119d5d
macs-code <omambiadauglous@gmail.com>
2019-06-13 12:52:11 +0300

    [chore] - rest Quesition 2 templates

---
+++ Quesition-2/app/helpers/generators.py
@@ -0,0 +1,71 @@
+import random 
+import os 
+import binascii
+
+# Main Helper to convert 
+
+def convert(num_list):
+    s  = [str(i) for i in num_list]
+    res  = int("".join(s))
+    return res 
+
+
+def MSISDN(num_list):
+    s  = [str(i) for i in num_list]
+    res  = str("".join(s))
+    return "2547" + str(res)
+
+def IMSI(num_list):
+    s  = [str(i) for i in num_list]
+    res  = str("".join(s))
+    return "63902" + str(res)
+
+def ICCID(num_list):
+    s  = [str(i) for i in num_list]
+    res  = int("".join(s))
+    return "8925402" + str(res)
+#get the pin foru digits 
+def get_pin():
+    number  = []
+    for x in range(5):
+        number.append(random.randint(0, 9))
+    code  = convert(number)
+    return code
+
+
+def get_puc():
+    number  = []
+    for x in range(7):
+        number.append(random.randint(0, 9))
+    code  = convert(number)
+    return code
+
+
+def get_ICCID():
+    number  = []
+    for x in range(16):
+        number.append(random.randint(0, 9))
+        code  = ICCID(number)
+    return code
+
+
+def get_IMSI():
+    number  = []
+    for x in range(16):
+        number.append(random.randint(0, 9))
+        code  = IMSI(number)
+    return code
+
+
+def get_Ki():
+    data  = binascii.b2a_hex(os.urandom(20))
+    return str(data)
+
+def get_MSISDN():
+    number  = []
+    for x in range(8):
+        number.append(random.randint(0, 9))
+        code  = MSISDN(number)
+    return code
+
+
--- Quesition-2/app/models/sim.py
+++ Quesition-2/app/models/sim.py
@@ -2,15 +2,19 @@ from  app import db
 import datetime 
 
 class SimCard(db.Model):
-    id         = db.Column(db.Integer, primary_key  = True,  unique = True, index  = True)
-    ICCID      = db.Column(db.String(20), unique = True)
-    IMSI       = db.Column(db.String(15), unique  = True) 
-    Ki         = db.Column(db.String(15), unique  = True) 
-    PIN1       = db.Column(db.String(15), unique  = True) 
-    PUC        = db.Column(db.String(15), unique  = True)   
+    id         = db.Column(db.Integer, primary_key  = True, index  = True)
+    ICCID      = db.Column(db.String(255), unique = True)
+    IMSI       = db.Column(db.String(255))
+    Ki         = db.Column(db.String(255))
+    PIN1       = db.Column(db.String(255))
+    PUC        = db.Column(db.String(255))  
     status     = db.Column(db.Integer, default  = 0)
-    MSISDN     = db.Column(db.String(12), unique = True)
     joined     = db.Column(db.DateTime, default = datetime.datetime.now)
+    MSISDN      = db.Column(db.String(12), unique = True)
+    balance     = db.Column(db.Integer, default  = 0)
+    
 
     def __str__(self):
-        return self.MSISDN     
+        return self.ICCID     
+
+
